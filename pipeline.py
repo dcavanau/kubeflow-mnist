abf016851e2f16af22c5fffa120478c8bc8979a0
David Cavanaugh <dcavanau@us.ibm.com>
2021-10-22 09:21:21 -0400

    change command strings

--- pipeline.py
+++ pipeline.py
@@ -1,21 +1,27 @@
 
 import kfp.dsl as dsl
 from kfp.dsl import PipelineVolume
-from kubernetes.client import V1VolumeMount
-from kubernetes.client import V1Volume
-from kubernetes.client import V1SecretVolumeSource
 
 # To compile the pipeline:
 #   dsl-compile --py pipeline.py --output pipeline.tar.gz
-from constants import PROJECT_ROOT, CONDA_PYTHON_CMD
+from constants import PROJECT_ROOT, PROJECT_NAME, CONDA_PYTHON_CMD
 
 
-def git_clone_op(repo_url: str, pvolume: PipelineVolume):
+def git_clone_op(repo_url: str):
     image = 'alpine/git:latest'
 
     commands = [
+        f"rm -rf {PROJECT_ROOT}",
         f"git clone {repo_url} {PROJECT_ROOT}",
-        f"cd {PROJECT_ROOT}"]
+        f"cd {PROJECT_ROOT}",
+        f"ls -lar"]
+
+    volume_op = dsl.VolumeOp(
+        name="create pipeline volume",
+        resource_name="pipeline-pvc",
+        modes=["ReadWriteOnce"],
+        size="3Gi"
+    )
 
     op = dsl.ContainerOp(
         name='git clone',
@@ -23,7 +29,7 @@ def git_clone_op(repo_url: str, pvolume: PipelineVolume):
         command=['sh'],
         arguments=['-c', ' && '.join(commands)],
         container_kwargs={'image_pull_policy': 'IfNotPresent'},
-        pvolumes={"/workspace": pvolume}
+        pvolumes={"/workspace": volume_op.volume}
     )
 
     return op
@@ -41,23 +47,39 @@ def preprocess_op(image: str, pvolume: PipelineVolume, data_dir: str):
 
 
 def train_and_eval_op(image: str, pvolume: PipelineVolume, data_dir: str):
+
+    commands = [
+        f"ls -lar"]
+        f"{CONDA_PYTHON_CMD} {PROJECT_ROOT}/train.py --data_dir={data_dir}",
+        f"cd {PROJECT_ROOT}",
+        f"ls -lar"]
+
     return dsl.ContainerOp(
         name='training and evaluation',
         image=image,
-        command=[CONDA_PYTHON_CMD, f"{PROJECT_ROOT}/train.py"],
-        arguments=["--data_dir", data_dir],
+        command=['sh'],
+        arguments=['-c', ' && '.join(commands)],
+#        command=[CONDA_PYTHON_CMD, f"{PROJECT_ROOT}/train.py"],
+#        arguments=["--data_dir", data_dir],
+        file_outputs={'output': f'{PROJECT_ROOT}/output.txt'},
         container_kwargs={'image_pull_policy': 'IfNotPresent'},
         pvolumes={"/workspace": pvolume}
     )
 
 
-def packaging(image: str, pvolume: PipelineVolume, model_path: str, model_name:str):
+def packaging(image: str, pvolume: PipelineVolume, model_path: str, model_name: str):
+    commands = [
+        f"tar -czvf {PROJECT_ROOT}/{PROJECT_NAME}.tar.gz -C {PROJECT_ROOT}",
+        f"cd {PROJECT_ROOT}",
+        f"ls -lar"]
     return dsl.ContainerOp(
         name='packaging',
         image=image,
-        command=[CONDA_PYTHON_CMD, f"{PROJECT_ROOT}/package.py"],
-        arguments=["--model_path", model_path, "--model_name", model_name],
- #       file_outputs={'output': f'{model_path}/{model_name}.tar'},
+        command=['sh'],
+        arguments=['-c', ' && '.join(commands)],
+#        command=[CONDA_PYTHON_CMD, f"{PROJECT_ROOT}/package.py"],
+#        arguments=["--model_path", model_path, "--model_name", model_name],
+#        file_outputs={'output': f'{model_path}/{model_name}.tar'},
         container_kwargs={'image_pull_policy': 'IfNotPresent'},
         pvolumes={"/workspace": pvolume}
     )
@@ -71,28 +93,20 @@ def training_pipeline(image: str = 'dcavanau/kubeflow-mnist',
                       repo_url: str = 'https://61acc7bc6d89fb89dffb2c7e2142adffef6b13f1:x-oauth-basic@github.com/dcavanau/kubeflow-mnist.git',
                       data_dir: str = '/workspace'):
 
-    _volume_op = dsl.VolumeOp(
-        name="create pipeline volume",
-        resource_name="pipeline-pvc",
-        modes=dsl.VOLUME_MODE_RWM,
-        size="3Gi",
-        storage_class="default"
-    )
-
-    _git_clone = git_clone_op(repo_url=repo_url, pvolume=_volume_op.volume)
+    _git_clone = git_clone_op(repo_url=repo_url)
 
     _preprocess_data = preprocess_op(image=image,
                                     pvolume=_git_clone.pvolume,
-                                    data_dir=data_dir).after(_git_clone)
+                                    data_dir=data_dir)
 
     _training_and_eval = train_and_eval_op(image=image,
                                            pvolume=_preprocess_data.pvolume,
-                                           data_dir=data_dir).after(_preprocess_data)
+                                           data_dir=data_dir)
 
     _packaging = packaging(image=image,
                            pvolume=_training_and_eval.pvolume,
                            model_path=data_dir,
-                           model_name='kubeflow-mnist').after(_training_and_eval)
+                           model_name='kubeflow-mnist')
 
 
 if __name__ == '__main__':
